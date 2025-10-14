0dd7148e0206a14c0b7030428ad42c34e42a4ebb
Anisur Rahman Naeem <anisurrahmannaeem84@gmail.com>
2025-10-14 23:52:07 +0600

    sign up screen done

--- firebase practice/intro/lib/ui/auth/login_screen.dart
+++ firebase practice/intro/lib/ui/auth/login_screen.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_connection/ui/auth/signup_screen.dart';
+import 'package:firebase_connection/widgets/round_button.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -8,22 +10,96 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
         title: Center(
-          child: Text("Login",style: TextStyle(
-            color: Colors.white
-          ),),
+          child: Text("Login", style: TextStyle(color: Colors.white)),
         ),
         backgroundColor: Colors.deepPurple,
       ),
-      body: Column(
-        children: [
-
-        ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    controller: emailController,
+                    decoration: const InputDecoration(
+                      hintText: 'Email',
+                      helperText: 'enter email e.g jon@gmail.com',
+                      prefixIcon: Icon(Icons.alternate_email),
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Enter email';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 10),
+                  TextFormField(
+                    keyboardType: TextInputType.text,
+                    controller: passwordController,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      hintText: 'Password',
+                      prefixIcon: Icon(Icons.alternate_email),
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Enter password';
+                      }
+                      return null;
+                    },
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 50),
+            RoundButton(
+              title: 'Login',
+              onTap: () {
+                if (_formKey.currentState!.validate()) {}
+              },
+            ),
+            const SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Don't have an account?"),
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SignUpScreen()),
+                    );
+                  },
+                  child: Text('Sign up'),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
