8556f48ce406b950ba75e16b794d63b8c0ed2285
Anisur Rahman Naeem <anisurrahmannaeem84@gmail.com>
2025-10-20 22:44:40 +0600

    firebase firestore CRUD done

--- firebase practice/intro/lib/firebase_services/splash_services.dart
+++ firebase practice/intro/lib/firebase_services/splash_services.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_connection/ui/auth/login_screen.dart';
+import 'package:firebase_connection/ui/firestore/firestore_list_screen.dart';
 import 'package:firebase_connection/ui/posts/post_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -16,7 +17,7 @@ class SplashServices {
 
     if(user != null){
       Timer(const Duration(seconds: 3),
-              ()=> Navigator.push(context, MaterialPageRoute(builder: (context) => PostScreen())));
+              ()=> Navigator.push(context, MaterialPageRoute(builder: (context) => FirestoreScreen())));
     }else{
       Timer(const Duration(seconds: 3),
               ()=> Navigator.push(context, MaterialPageRoute(builder: (context) => LoginScreen())));
---
+++ firebase practice/intro/lib/ui/firestore/firestore_list_screen.dart
@@ -0,0 +1,158 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_connection/ui/auth/login_screen.dart';
+import 'package:firebase_connection/ui/firestore/add_firestore_data.dart';
+import 'package:firebase_connection/ui/posts/add_posts.dart';
+import 'package:firebase_connection/utlis/utlis.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
+import 'package:flutter/material.dart';
+
+class FirestoreScreen extends StatefulWidget {
+  const FirestoreScreen({super.key});
+
+  @override
+  State<FirestoreScreen> createState() => _FirestoreScreenState();
+}
+
+class _FirestoreScreenState extends State<FirestoreScreen> {
+  final auth = FirebaseAuth.instance;
+  final editController = TextEditingController();
+  final fireStore = FirebaseFirestore.instance.collection('users').snapshots();
+
+  CollectionReference ref = FirebaseFirestore.instance.collection('users');
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        centerTitle: true,
+        title: Text('Firestore'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              auth
+                  .signOut()
+                  .then((value) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => LoginScreen()),
+                    );
+                  })
+                  .onError((error, stackTrace) {
+                    Utils().toastMessage(error.toString());
+                  });
+            },
+            icon: Icon(Icons.logout_outlined),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AddFirestoreDataScreen()),
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+      body: Column(
+        children: [
+          // Expanded(
+          //     child: StreamBuilder(
+          //       stream: ref.onValue,
+          //         builder: (context, AsyncSnapshot<DatabaseEvent>snapshot){
+          //           if(!snapshot.hasData){
+          //             return CircularProgressIndicator();
+          //           }
+          //           else {
+          //             Map<dynamic, dynamic> map = snapshot.data!.snapshot.value as dynamic;
+          //             List<dynamic> list = [];
+          //             list.clear();
+          //             list = map.values.toList();
+          //
+          //             return ListView.builder(
+          //                 itemCount: snapshot.data!.snapshot.children.length,
+          //                 itemBuilder: (context, index){
+          //                   return ListTile(
+          //                     title: Text(list[index]['title']),
+          //                     subtitle: Text(list[index]['id']),
+          //                   );
+          //                 });
+          //           }
+          //         })),
+          SizedBox(height: 10),
+          StreamBuilder<QuerySnapshot>(
+            stream: fireStore,
+            builder: (
+              BuildContext context,
+              AsyncSnapshot<QuerySnapshot> snapshot,
+            ) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return CircularProgressIndicator();
+              }
+              if (snapshot.hasError) {
+                return Text('Some error');
+              }
+              return Expanded(
+                child: ListView.builder(
+                  itemCount: snapshot.data!.docs.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      onTap: (){
+                        ref.doc(snapshot.data!.docs[index]['id'].toString()).update({
+                          'title' : 'subscribe'
+                        }).then((value){
+                          Utils().toastMessage('updated');
+                        }).onError((error, stackTrace){
+                          Utils().toastMessage(error.toString());
+                        });
+
+                        ref.doc(snapshot.data!.docs[index]['id'].toString()).delete();
+                      },
+                      title: Text(snapshot.data!.docs[index]['title'].toString()),
+                      subtitle: Text(snapshot.data!.docs[index]['id'].toString()),
+                    );
+                  },
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> showMyDialog(String title, String id) async {
+    editController.text = title;
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Update'),
+          content: Container(
+            child: TextField(
+              controller: editController,
+              decoration: InputDecoration(hintText: 'Edit'),
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Update'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
